437e61c729296fa063f5d5e9ff6ed99f88483d82
Ricardo Dias <slo.motion@gmail.com>
2022-12-18 15:23:57 +0000

    First commit with first steps

---
+++ resources/js/Pages/Blood.vue
@@ -0,0 +1,49 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import {Head} from '@inertiajs/inertia-vue3';
+</script>
+
+<template>
+    <Head title="Dashboard"/>
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Blood pressure records</h2>
+        </template>
+
+        <div class="m-2 grid content-center border border-gray-300 rounded-md overflow-hidden">
+
+            <table class="min-w-full table-fixed text-sm bg-white">
+                <thead class="h-12 bg-gray-50 text-left border-b">
+                <tr class="">
+                    <th class="px-4 ">Date</th>
+                    <th class="px-4">High Value</th>
+                    <th class="px-4">Low Value</th>
+                    <th class="px-4">Bps</th>
+                </tr>
+                </thead>
+                <tbody class="h-28 divide-y divide-gray-100 text-md">
+                <tr class="hover:bg-gray-100">
+                    <td class="px-4">{{ new Intl.DateTimeFormat('pt-PT').format(new Date) }}</td>
+                    <td class="px-4">12</td>
+                    <td class="px-4">6.5</td>
+                    <td class="px-4">74</td>
+                </tr>
+                <tr class="hover:bg-gray-100">
+                    <td class="px-4">{{ new Intl.DateTimeFormat('pt-PT').format(new Date) }}</td>
+                    <td class="px-4">12</td>
+                    <td class="px-4">6.5</td>
+                    <td class="px-4">74</td>
+                </tr>
+                <tr class="hover:bg-gray-100">
+                    <td class="px-4">{{ new Intl.DateTimeFormat('pt-PT').format(new Date) }}</td>
+                    <td class="px-4">12</td>
+                    <td class="px-4">6.5</td>
+                    <td class="px-4">74</td>
+                </tr>
+
+                </tbody>
+            </table>
+        </div>
+    </AuthenticatedLayout>
+</template>
---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,29 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import {Head} from '@inertiajs/inertia-vue3';
+import {Link} from "@inertiajs/inertia-vue3";
+</script>
+
+<template>
+    <Head title="Dashboard"/>
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-t-lg">
+                    <div class="p-6 text-gray-900">This are your enabled services:</div>
+                </div>
+
+                <div class="px-4 bg-gray-50 overflow-hidden shadow-sm sm:rounded-b-lg">
+                    <div class="p-6 text-gray-900">
+                        <Link href="/blood">Blood pressure</Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
