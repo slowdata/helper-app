75c465e1c4c996190f0ee6c4b8a99bf6f2ef3483
Ricardo Dias <slo.motion@gmail.com>
2022-12-30 23:43:30 +0000

    Migration, model and relationship to be able to create and manage blood pressure measures

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function bloodpressuremeasures()
+    {
+        return $this->hasMany(BloodPressureMeasure::class);
+    }
 }
