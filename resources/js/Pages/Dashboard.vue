1108ca5222ed12e0b9627167a57c1ad9dbc10455
Ricardo Dias <slo.motion@gmail.com>
2023-01-02 01:38:26 +0000

    Changed text for better understanding

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -16,7 +16,7 @@ import { Link } from "@inertiajs/inertia-vue3";
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-t-lg">
-                    <div class="p-6 text-gray-900">This are your enabled services:</div>
+                    <div class="p-6 text-gray-900">These are your available services:</div>
                 </div>
 
                 <div class="p-2 bg-white overflow-hidden shadow-sm sm:rounded-b-lg">
